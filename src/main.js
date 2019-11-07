e3f22dc1285cdd07a5f62b49b1f3716691e91bc9
marcopavan <m.pavan@neticon.it>
2019-11-07 18:04:26 +0100

    Vuex

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Vuex from 'vuex'
 import BootstrapVue from 'bootstrap-vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
@@ -8,7 +9,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 import CmpHome from './components/sections/home/CmpHome.vue';
 import CmpTour from './components/sections/tour/CmpTour.vue';
 
-
+Vue.use(Vuex)
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 
@@ -23,7 +24,19 @@ const router = new VueRouter({
   routes 
 });
 
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
 new Vue({
   render: h => h(App),
   router,
+  store,
 }).$mount('#app')
